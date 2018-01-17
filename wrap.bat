25e8e363766c029c3644f0bbd59a4706e6c90396
g10guang <g10guang@foxmail.com>
2018-01-17 14:40:37 +0800

    重新打包只有一个执行文件 WHUT.exe

--- wrap.bat
+++ wrap.bat
@@ -1,1 +1,6 @@
-pyinstaller --add-data assert\*;assert --distpath F:\tmp\grabcourses --paths=F:\code\python\projects\grabCourses\env\Scripts --paths F:\code\python\projects\grabCourses\env\Lib\site-packages -w .\run.py
+# product
+pyinstaller --add-data assert\*;assert --distpath F:\tmp\grabcourses --paths=F:\code\python\projects\grabCourses\env\Scripts --paths F:\code\python\projects\grabCourses\env\Lib\site-packages -w --name WHUT --onefile .\run.py
+
+
+# test
+pyinstaller --add-data assert\*;assert --distpath F:\tmp\grabcourses --paths=F:\code\python\projects\grabCourses\env\Scripts --paths F:\code\python\projects\grabCourses\env\Lib\site-packages .\run.py
